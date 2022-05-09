71fc26145f5637aae8fb2a8382ba4fb6a8e5f2ad
MDCarrasco <faultyquisby@protonmail.com>
2022-05-09 13:37:33 +0200

    added test folder, ready for testing

---
+++ test/test_unit.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class ServerTester(unittest.TestCase):
+    def setup(self):
+        pass
+
+    def test_1(self):
+        pass
+
+    def test_2(self):
+        pass
+
+    def test_3(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
